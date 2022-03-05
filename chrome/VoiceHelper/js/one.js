7002421badc4f7c9f5abc4bb841e79ac512ac45d
fxqy <cectdwf@126.com>
2022-03-05 16:36:53 +0800

    vocchg

--- chrome/VoiceHelper/js/one.js
+++ chrome/VoiceHelper/js/one.js
@@ -1,7 +1,8 @@
-
+$ReqUrl="";
 window.onload=function(){
 	initTabs();
 	initEvents();
+	initListener();
 };
 
 function initTabs(){
@@ -40,16 +41,13 @@ function initEvents(){
 	//a
 	_$G("taba_btn1").onclick=function(){
 		_$G("pgctt_txts").value="";
-		$ContentStack=[];
-		$ContentDatas=[];
-		$LastCode="0";
 		tipCase({msg:"Done"});
 	};
 	_$G("taba_btn2").onclick=function(){
 
-        //var txtarea = _$G("pgctt_txts");
-		//txtarea.value=JSON.stringify(newarr);
-        tipCase({msg:"taba_btn2 Clicked..."});
+        var txtarea = _$G("pgctt_txts");
+		txtarea.value=$ReqUrl;
+        //tipCase({msg:"taba_btn2 Clicked..."});
         
 	};
 	//b
@@ -66,15 +64,48 @@ function initEvents(){
 			}
 		});
 	};
-	//listeners
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-		//console.log(message);
-		if(message.id=="content_page"){
-                sendResponse({text:"唐门外门弟子唐三，因偷学内门绝学为唐门所不容，跳崖明志时却发现没有死，反而以另外一个身份来到了另一个世界，一个属于武魂的世界，名叫斗罗大陆。这里没有魔法，没有斗气，没有武术，却有神奇的武魂。这里的每个人，在自己六岁的时候，都会在武魂殿中令武魂觉醒。武魂有动物，有植物，有器物，武魂可以辅助人们的日常生活。而其中一些特别出色的武魂却可以用来修炼并进行战斗，这个职业，是斗罗大陆上最为强大也是最荣耀的职业魂师当唐门暗器来到斗罗大陆，当唐三武魂觉醒，他能否在这片武魂的世界再铸唐门的辉煌他能否成为这个世界的主宰"});
-                
-		}
-	});
+
     
 }
 
+function initListener(){
+
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+		switch(message.id){
+		    case "on_load":
+		        on_ctt_load();
+				sendResponse({state:"ok"});
+		        break;
+		    case "get_state":
+		        sendResponse({state:"ok"});
+		        break;
+		    default:  
+				sendResponse({text:""});
+		}
+	});
+	
+	chrome.webRequest.onBeforeRequest.addListener(//https://audiopay.cos.tx.xmcdn.com/download/1.0.0/group2/M07/08/E0/wKgLdFw1gXOSvj3gAKIwSfXmO0U608.m4a
+		onBeforeSendRequest,
+		{urls:["*://audiopay.cos.tx.xmcdn.com/download/*"]},
+		["blocking"]
+	);
+}
+function on_ctt_load(){
+    
+}
+
+
+
+function onBeforeSendRequest(dtl){
+	if("media"==dtl.type && dtl.url.indexOf(".m4a")>-1){
+		$ReqUrl=dtl.url;
+	}
+	
+	
+	/**
+	if(dtl.url.indexOf("qq.com")>-1){
+		return {cancel: true};
+	}
+	*/
+}
 
